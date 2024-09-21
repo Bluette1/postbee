670ba9dce301b16146a2aec42408ffed56750216
Bluette1 <marylene.sawyer@gmail.com>
2024-09-22 00:26:56 +0200

    Add logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AuthService } from '../auth.service'; 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn; 
+  }
 
   navigateToLogin() {
     this.router.navigate(['/login']);
   }
 
   navigateToSignUp() {
-    this.router.navigate(['/sign-up']); 
+    this.router.navigate(['/sign-up']);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']); // Optionally redirect to home after logout
   }
 }
