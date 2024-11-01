2d57c1b4b237fa6cf9804d8c4363091091302578
Bluette1 <marylene.sawyer@gmail.com>
2024-11-01 17:44:30 +0200

    chore: Use Environment Variables
    - Use Environment Variables
       - Preparation for deployment

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,12 +4,13 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AuthResponse } from './auth-response.interface';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = environment.apiUrl; // Use environment variable
 
   constructor(private http: HttpClient, private router: Router) {}
 
