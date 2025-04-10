14b7c2ac35ae57aafdfeaa90a87f7e1bdf10dde7
Bluette1 <marylene.sawyer@gmail.com>
2025-04-10 17:32:31 +0200

    fix: Netlify Build
    - Fix Netlify Build
    
    issue #10

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://poster-33fdb2083be4.herokuapp.com',
+  trackerApiUrl: 'https://postbee-tracker-api-438f610a3ed6.herokuapp.com',
+  aws: {
+    accessKeyId: 'YOUR_ACCESS_KEY_ID',
+    secretAccessKey: "YOUR_SECRET_ACCESS_KEY",
+    region: 'us-east-1',
+    bucketName: 'postbee-store'
+  },
+  calendlyUrl: 'https://calendly.com/marylene-sawyer/15min'
+};
