a15470519ebe69b454d55cba5dc8f10ca918954f
Bluette1 <marylene.sawyer@gmail.com>
2025-04-10 17:59:51 +0200

    chore: Clean Up
    - Clean Up
    
    Issue #10

--- set-env.js
+++ set-env.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const envFilePath = path.join(__dirname, 'src/environments/environment.production.ts');
+const envFilePath = path.join(
+  __dirname,
+  "src/environments/environment.production.ts"
+);
 
-let content = fs.readFileSync(envFilePath, 'utf8');
-
-console.log('AWS_ACCESS_PUBLIC_KEY:', process.env.AWS_ACCESS_PUBLIC_KEY);
-console.log('AWS_SECRET_PRIVATE_KEY:', process.env.AWS_SECRET_PRIVATE_KEY);
-
+let content = fs.readFileSync(envFilePath, "utf8");
 
 content = content
-  .replace('YOUR_ACCESS_KEY_ID', process.env.AWS_ACCESS_PUBLIC_KEY)
-  .replace('YOUR_SECRET_ACCESS_KEY', process.env.AWS_SECRET_PRIVATE_KEY);
+  .replace("YOUR_ACCESS_KEY_ID", process.env.AWS_ACCESS_PUBLIC_KEY)
+  .replace("YOUR_SECRET_ACCESS_KEY", process.env.AWS_SECRET_PRIVATE_KEY);
 
-fs.writeFileSync(envFilePath, content, 'utf8');
+fs.writeFileSync(envFilePath, content, "utf8");
