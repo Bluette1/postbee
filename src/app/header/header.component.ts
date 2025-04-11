533eaa3d55b17b2db021aa545686928e0a727ed3
Bluette1 <marylene.sawyer@gmail.com>
2025-04-11 18:58:27 +0200

    feat: Responsive Navbar
    - Add Responsive Navbar
    
    Issue #13

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service'; 
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  // Add this property
+  isMenuOpen = false;
   constructor(private router: Router, private authService: AuthService) {}
 
   get isLoggedIn(): boolean {
-    return this.authService.isLoggedIn; 
+    return this.authService.isLoggedIn;
   }
 
   navigateToLogin() {
@@ -26,5 +28,24 @@ export class HeaderComponent {
 
   navigateToPostJob() {
     this.router.navigate(['/job-posts/new']);
+  }
+
+  navigateToIntro() {
+    this.router.navigate(['/employers/intro']);
+  }
+
+  navigateToHireTalent() {
+    this.router.navigate(['/employers/hire-talent']);
+  }
+  navigateToGuide() {
+    this.router.navigate(['/job-seekers/guide']);
+  }
+
+  navigateToJournal() {
+    this.router.navigate(['/resources/journal']);
+  }
+
+  navigateToDashboard() {
+    this.router.navigate(['/resources/hunterboard']);
   }
 }
