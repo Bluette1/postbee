cb10ca4c07c4a325c55e9cbddb275f7d62419564
Bluette1 <marylene.sawyer@gmail.com>
2025-04-11 20:40:55 +0200

    feat: Responsive Navbar
    - Add Responsive Navbar
    
    Issue #13

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   // Add this property
