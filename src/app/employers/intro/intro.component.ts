8be7f98a7379dde555a2f3808998d4a0caada89e
Bluette1 <marylene.sawyer@gmail.com>
2025-04-14 22:08:11 +0200

    feat: Styles Intro
    - Styles Intro
    
    Issue #13

--- src/app/employers/intro/intro.component.ts
+++ src/app/employers/intro/intro.component.ts
@@ -1,28 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit, OnDestroy {
   testimonials = [
-    { picture: 'assets/testimonial1.jpg', remarks: 'Great platform for finding remote talent!' },
-    { picture: 'assets/testimonial2.jpg', remarks: 'Found my dream job within a week!' },
-    { picture: 'assets/testimonial3.jpg', remarks: 'The hybrid options were perfect for our company needs.' }
+    {
+      picture: 'assets/images/gettyimages-professional-1.avif',
+      remarks: 'Great platform for finding remote talent!',
+    },
+    {
+      picture: 'assets/images/gettyimages-professional-2.avif',
+      remarks: 'Found a match within a week!',
+    },
+    {
+      picture: 'assets/images/gettyimages-professional-3.avif',
+      remarks: 'The hybrid options were perfect for our company needs.',
+    },
   ];
-  
+
   currentTestimonialIndex = 0;
-  
+  intervalId: any;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
   navigateToPostJob() {
-    // Your navigation logic here
+    this.router.navigate(['/job-posts/new']);
   }
-  
+
   nextTestimonial() {
-    this.currentTestimonialIndex = (this.currentTestimonialIndex + 1) % this.testimonials.length;
+    this.currentTestimonialIndex =
+      (this.currentTestimonialIndex + 1) % this.testimonials.length;
   }
-  
+
   prevTestimonial() {
-    this.currentTestimonialIndex = (this.currentTestimonialIndex - 1 + this.testimonials.length) % this.testimonials.length;
+    this.currentTestimonialIndex =
+      (this.currentTestimonialIndex - 1 + this.testimonials.length) %
+      this.testimonials.length;
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.nextTestimonial();
+    }, 5000); // Change testimonial every 5 seconds
+  }
+
+  stopAutoSlide() {
+    clearInterval(this.intervalId);
   }
 }
